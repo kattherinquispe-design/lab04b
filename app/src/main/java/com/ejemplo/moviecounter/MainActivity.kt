a7b1ae273e2e7d901cdfca9c0c820418be56b3c1
kattherin <kattherin@gmail.com>
2026-09-08 16:14:59 -0500

    feat: Implement MovieCounter with rememberSaveable

--- app/src/main/java/com/ejemplo/moviecounter/MainActivity.kt
+++ app/src/main/java/com/ejemplo/moviecounter/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.ejemplo.moviecounter
-
+import androidx.compose.runtime.saveable.rememberSaveable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,9 +49,8 @@ fun MovieCounter(modifier: Modifier = Modifier) {
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun PreviewMovieCounter() {
     MovieCounter()
-}  
+}
